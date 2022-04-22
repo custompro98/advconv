80a4478f478652633c56ce51ea4bfbd256646a74
custompro98 <mitchjoa@gmail.com>
2022-04-22 16:49:12 -0400

    more docs

--- internal/pkg/adventure/adventure.go
+++ internal/pkg/adventure/adventure.go
@@ -37,6 +37,7 @@ type EntryJSON struct {
 	Entries []Entry `json:"entries"`
 }
 
+// Parse turns a JSON string from 5eTools into an @Adventure
 func Parse(s string) (Adventure, error) {
 	var adventure Adventure
 
